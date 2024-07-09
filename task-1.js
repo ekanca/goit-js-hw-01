7991a6acd2be737541ce8bc1689b6b3ae1df0999
Ekanca <emrekanca@gmail.com>
2024-07-09 14:17:55 +0300

    task 1 create

--- task-1.js
+++ task-1.js
@@ -1,1 +1,26 @@
+const quantity = 5;
+const pricePerDroid = 3000;
+const makeTransaction = quantity * pricePerDroid;
+
+console.log(makeTransaction);
+console.log("You ordered " + quantity + " droids worth " + makeTransaction + " credits!");
+/*
+const quantity = 3;
+const pricePerDroid = 1000;
+const makeTransaction = quantity * pricePerDroid;
+
+console.log(makeTransaction);
+console.log("You ordered " + quantity + " droids worth " + makeTransaction + " credits!");
+
+const quantity = 10;
+const pricePerDroid = 500;
+const makeTransaction = quantity * pricePerDroid;
+
+console.log(makeTransaction);
+console.log("You ordered " + quantity + " droids worth " + makeTransaction + " credits!");
+
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+*/
 
