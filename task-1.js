2cb2e36b305369fc72f783bef16caa9c5e4e6e0e
Ekanca <emrekanca@gmail.com>
2024-07-09 19:43:26 +0300

    task 1-2-3 edit

--- task-1.js
+++ task-1.js
@@ -1,26 +1,8 @@
-const quantity = 5;
-const pricePerDroid = 3000;
-const makeTransaction = quantity * pricePerDroid;
-
-console.log(makeTransaction);
-console.log("You ordered " + quantity + " droids worth " + makeTransaction + " credits!");
-/*
-const quantity = 3;
-const pricePerDroid = 1000;
-const makeTransaction = quantity * pricePerDroid;
-
-console.log(makeTransaction);
-console.log("You ordered " + quantity + " droids worth " + makeTransaction + " credits!");
-
-const quantity = 10;
-const pricePerDroid = 500;
-const makeTransaction = quantity * pricePerDroid;
-
-console.log(makeTransaction);
-console.log("You ordered " + quantity + " droids worth " + makeTransaction + " credits!");
+function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-*/
-
